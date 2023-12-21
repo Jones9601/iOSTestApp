720856df593463e0eeeaccb90fb438523e6ca19d
Jones <jones@m2pfintech.com>
2023-12-21 17:01:01 +0530

    completed transaction view model test case

--- ApiCallSwiftUITests/CommentViewModelTests.swift
+++ ApiCallSwiftUITests/CommentViewModelTests.swift
@@ -3,9 +3,13 @@ import XCTest
 
 class CommentViewModelTests: XCTestCase {
     func testComments() {
-        let sut = CommentViewModel(commentViewService: MockCommentViewService())
-        sut.fetchComments()
-        print("========= >11 \(sut.comments)")
-        XCTAssert(sut.comments.count == 500)
+        guard let fileUrl = Bundle.main.url(forResource: "issuer", withExtension: "json") else {
+            return
+        }
+        print("fileUrl", fileUrl)
+//        let sut = CommentViewModel(commentViewService: MockCommentViewService())
+//        sut.fetchComments()
+//        print("========= >11 \(sut.comments)")
+//        XCTAssert(sut.comments.count == 500)
     }
 }
